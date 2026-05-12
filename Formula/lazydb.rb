f59d31df6b364f7bb9db9db12efd3e0a611e4f00
古川拓矢 <furukawatakuya@furukawatakuyanoMacBook-Pro.local>
2026-05-12 18:53:29 +0900

    lazydb 0.2.4

--- Formula/lazydb.rb
+++ Formula/lazydb.rb
@@ -1,28 +1,28 @@
 class Lazydb < Formula
   desc "Standalone TUI SQL client with vim keybindings"
   homepage "https://github.com/takuyatechexpert/lazydb"
-  version "0.2.3"
+  version "0.2.4"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/takuyatechexpert/lazydb/releases/download/v0.2.2/lazydb-aarch64-apple-darwin.tar.gz"
-      sha256 "5578f847a800cbdbd4fad1ef68c6b135ee66b0818fb489c5cfd9a7e2bf7ae973"
+      url "https://github.com/takuyatechexpert/lazydb/releases/download/v0.2.4/lazydb-aarch64-apple-darwin.tar.gz"
+      sha256 "d251718330ae9b4323f7e3096f9ab3291d881c2ac6393db01adc8ca39b06219b"
     end
     on_intel do
-      url "https://github.com/takuyatechexpert/lazydb/releases/download/v0.2.2/lazydb-x86_64-apple-darwin.tar.gz"
-      sha256 "457e1b33774a24d13fb5c98101581ff0f04db584050d988ab93de77d4207203b"
+      url "https://github.com/takuyatechexpert/lazydb/releases/download/v0.2.4/lazydb-x86_64-apple-darwin.tar.gz"
+      sha256 "c4e1d0f05a24a79067ab28335743e28128d7ee404a6705a62eb7bf2806545f0b"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/takuyatechexpert/lazydb/releases/download/v0.2.2/lazydb-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "9e8b0d35738e3b078845e976e27341cc4f2fff7dd021eee4303e9798537baf06"
+      url "https://github.com/takuyatechexpert/lazydb/releases/download/v0.2.4/lazydb-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "55698abaa9c6365e44ad26119ae681eac02c105705f59e88bd7bf64f6ca9ef2f"
     end
     on_intel do
-      url "https://github.com/takuyatechexpert/lazydb/releases/download/v0.2.3/lazydb-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "100368ea72d5c30a10b47e1cf5285fa6b454f545ea2d33483d3a46a8f75aff00"
+      url "https://github.com/takuyatechexpert/lazydb/releases/download/v0.2.4/lazydb-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "154a7f47ead97b2dfb5fbc39261d9caeb1aa219129bbbcb2ee32b5b10a2c64a0"
     end
   end
 
